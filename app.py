ef9d7c7fbc0506a4c3a4215e64a667cdef71a92a
balcenajessica <balcenajessicaann@isufst.edu.ph>
2026-09-20 09:33:07 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,61 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Welcome to my first API!"
+
+@app.route('/student')
+def get_student():
+    return jsonify({
+        "student_id": "24-00268",
+        "name": "Jessica Ann Balcena",
+        "program": "BSIT",
+        "year": 3,
+        "section": "A"
+    })
+
+@app.route('/hello')
+def say_hello():
+    name = request.args.get('name', 'Student')
+    return jsonify({
+        "message": f"Hello, {name}!"
+    })
+
+@app.route('/course')
+def get_course():
+    return jsonify({
+        "course_code": "IT3120",
+        "course_title": "System Integration",
+        "instructor": "Dr. Rene Arduo",
+        "semester": "First Semester",
+        "academic_year": "2026-2027"
+    })
+
+@app.route('/greet')
+def greet():
+    name = request.args.get('name', 'Student')
+    section = request.args.get('section', 'BSIT3A')
+    return jsonify({
+        "message": f"Hello {name} from {section}!"
+    })
+
+@app.route('/profile')
+def profile():
+    return jsonify({
+        "student_id": "24-00268",
+        "name": "Jessica Ann Balcena",
+        "program": "BSIT",
+        "year": 3,
+        "section": "A",
+        "email": "balcenajessicaann@isufst.edu.ph",
+        "skills": [
+            "Python",
+            "HTML",
+            "Networking"
+        ]
+    })
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=True)
